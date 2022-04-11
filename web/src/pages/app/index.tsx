48c7165266edfcf4947b1ce4bdfe38379f89bee0
Victor Epifânio <vepifanio1@gmail.com>
2022-04-11 20:24:13 -0300

    New commit

--- web/src/pages/app/index.tsx
+++ web/src/pages/app/index.tsx
@@ -1,20 +1,28 @@
 import {
-	useUser,
-	withPageAuthRequired,
-	getAccessToken,
+  useUser,
+  withPageAuthRequired,
+  getAccessToken,
 } from "@auth0/nextjs-auth0";
 import { GetServerSideProps } from "next";
 
 export default function Home() {
-	const { user } = useUser();
+  const { user } = useUser();
 
-	return (
-		<div>
-			<h1>Hello World</h1>
+  return (
+    <div>
+      <h1>Hello World</h1>
 
-			<pre>{JSON.stringify(user, null, 2)}</pre>
-		</div>
-	);
+      <pre>{JSON.stringify(user, null, 2)}</pre>
+    </div>
+  );
 }
 
 export const getServerSideProps = withPageAuthRequired();
+
+// export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+//   console.log(getAccessToken(req, res));
+
+//   return {
+//     props: {},
+//   };
+// };
